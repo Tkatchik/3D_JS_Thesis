015b3899d951d14f548bbe0fbfd3b26ddbb1fd29
tkatchik <tkatchik@gmail.com>
2019-08-09 20:01:45 +0200

    .

--- src/modules/popupDiscount.js
+++ src/modules/popupDiscount.js
@@ -6,23 +6,27 @@ const popupDiscount = () => {
             message.parentNode.removeChild(message);
         }
     };
-    const checkBtn = document.querySelector('.check-button');
-    checkBtn.addEventListener('click', (event) => {
+    const sentence = document.querySelector('.sentence');
+    sentence.addEventListener('click', (event) => {
         event.preventDefault();
+        let target = event.target;
 
-        const popupDisc = document.querySelector('.popup-discount');
-        popupDisc.style.display = 'block';
+        if (target.matches('.sentence-btn')) {
+            const popup = document.querySelector('.popup-discount');
+            popup.style.display = 'block';
 
-        popupDisc.addEventListener('click', (event) => {
-            let target = event.target;
-            if (target.matches('.popup-close')) {
-                popupDisc.style.display = 'none';
-                statusMess();
-            } else if (target.matches(".popup-discount:not(.popup-content)")) {
-                popupDisc.style.display = 'none';
-                statusMess();
-            }
-        });
+            popup.addEventListener('click', (event) => {
+                let target = event.target;
+
+                if (target.matches('.popup-close')) {
+                    popup.style.display = 'none';
+                    statusMess();
+                } else if (target.matches('.popup-discount:not(.popup-content)')) {
+                    popup.style.display = 'none';
+                    statusMess();
+                }
+            });
+        }
     });
 };
 export default popupDiscount;
